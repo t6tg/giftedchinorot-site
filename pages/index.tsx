5d64ecc7f0a850bf4f690119fde49c0d709eb770
thanawatgulati <thanawatgulatijames@gmail.com>
2020-06-03 00:30:43 +0700

    💅 TailwindCSS Config

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,5 +3,11 @@ import React, { ReactElement } from "react";
 interface Props {}
 
 export default function index({}: Props): ReactElement {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <button className="bg-blue-500 py-2 rounded-md text-white px-4">
+        Click Me!
+      </button>
+    </div>
+  );
 }
